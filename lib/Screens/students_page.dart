06cfb52b755eac78ce9e2c7cbe8c6e96a55d0201
Jery-VB <jery99961@gmail.com>
2024-01-07 01:44:52 +0530

    Fully Fix Room Cards

--- lib/Screens/students_page.dart
+++ lib/Screens/students_page.dart
@@ -1,9 +1,12 @@
 // ignore_for_file: prefer_const_constructors
 
+import 'dart:math';
+
 import 'package:card_loading/card_loading.dart';
 import 'package:flutter/material.dart';
 import 'package:hostel_management_application/Components/contact_card.dart';
 import 'package:hostel_management_application/Components/random_person_icon.dart';
+import 'package:url_launcher/url_launcher_string.dart';
 
 import '../Components/edit_student_dialog.dart';
 import '../Components/filter_students_dialog.dart';
@@ -24,7 +27,7 @@ class _StudentsPageState extends State<StudentsPage> {
   final DatabaseService dbService = DatabaseService();
   final searchController = TextEditingController();
   late Future<List<Student>> students = refreshStudents();
-  final List<bool> _filterOpts = List.generate(10, (_) => false);
+  final List<bool> _filterOpts = List.generate(20, (_) => false);
 
   Future<List<Student>> refreshStudents([bool getFromCache = false]) {
     if (widget.hostel != null) {
@@ -69,49 +72,12 @@ class _StudentsPageState extends State<StudentsPage> {
                         mainAxisSize: MainAxisSize.min,
                         children: [
                           IconButton(
-                            onPressed: () {
-                              showModalBottomSheet(
-                                context: context,
-                                builder: (BuildContext context) {
-                                  return FilterOptionsDialog(
-                                    selections: _filterOpts,
-                                    onSelectionChanged: (opts) {
-                                      students = refreshStudents(true)
-                                          .then((studentList) {
-                                        List<Student> filtered = List.empty();
-                                        filtered += studentList
-                                            .where((s) =>
-                                                opts[0] && s.year == 2021 ||
-                                                opts[1] && s.year == 2022 ||
-                                                opts[2] && s.year == 2023 ||
-                                                opts[3] && s.year == 2024 ||
-                                                opts[4] && s.floor == 1 ||
-                                                opts[5] && s.floor == 2 ||
-                                                opts[6] && s.floor == 3 ||
-                                                opts[7] && s.gender == 'male' ||
-                                                opts[8] && s.gender == 'female')
-                                            .toSet()
-                                            .toList();
-                                        return filtered;
-                                      });
-                                      setState(() {
-                                        students = opts.contains(true)
-                                            ? students
-                                            : refreshStudents(true);
-                                      });
-                                    },
-                                  );
-                                },
-                              );
-                            },
-                            icon: Icon(Icons.filter_list_alt),
-                          ),
+                              onPressed: () => showFilterDialog(context),
+                              icon: Icon(Icons.filter_list_alt)),
                           IconButton(
                             onPressed: () {
                               searchController.clear();
-                              setState(() {
-                                students = refreshStudents(true);
-                              });
+                              setState(() => students = refreshStudents(true));
                             },
                             icon: Icon(Icons.clear),
                           ),
@@ -152,6 +118,39 @@ class _StudentsPageState extends State<StudentsPage> {
     );
   }
 
+  Future<dynamic> showFilterDialog(BuildContext context) {
+    return showModalBottomSheet(
+      context: context,
+      builder: (BuildContext context) {
+        return FilterOptionsDialog(
+          selections: _filterOpts,
+          onSelectionChanged: (opts) {
+            students = refreshStudents(true).then((studentList) {
+              List<Student> filtered = List.empty();
+              filtered += studentList
+                  .where((s) =>
+                      opts[0] && s.year == 2021 ||
+                      opts[1] && s.year == 2022 ||
+                      opts[2] && s.year == 2023 ||
+                      opts[3] && s.year == 2024 ||
+                      opts[4] && s.floor == 1 ||
+                      opts[5] && s.floor == 2 ||
+                      opts[6] && s.floor == 3 ||
+                      opts[7] && s.gender == 'male' ||
+                      opts[8] && s.gender == 'female')
+                  .toSet()
+                  .toList();
+              return filtered;
+            });
+            setState(() {
+              students = opts.contains(true) ? students : refreshStudents(true);
+            });
+          },
+        );
+      },
+    );
+  }
+
   Widget _buildDefaultView() {
     return FutureBuilder<List<Student>>(
       future: students,
@@ -189,13 +188,13 @@ class _StudentsPageState extends State<StudentsPage> {
         if (snapshot.connectionState == ConnectionState.waiting) {
           return SliverGrid(
             gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                crossAxisCount: MediaQuery.of(context).size.width ~/ 160),
+                crossAxisCount: MediaQuery.of(context).size.width ~/ 140),
             delegate: SliverChildBuilderDelegate(
               (BuildContext context, int index) {
                 return CardLoading(
-                    height: 120, child: verticalStudentCard(Student.empty()));
+                    height: 140, child: verticalStudentCard(Student.empty()));
               },
-              childCount: 30,
+              childCount: 15,
             ),
           );
         } else if (snapshot.hasError) {
@@ -204,8 +203,8 @@ class _StudentsPageState extends State<StudentsPage> {
         } else {
           final students = snapshot.data!;
 
-          // Group students by room number
-          final studentsMap = <int, List<Student>>{};
+          // Group students by room number and arrange in ascending order
+          var studentsMap = <int, List<Student>>{};
           for (var student in students) {
             if (studentsMap.containsKey(student.room)) {
               studentsMap[student.room]!.add(student);
@@ -213,17 +212,20 @@ class _StudentsPageState extends State<StudentsPage> {
               studentsMap[student.room] = [student];
             }
           }
+          final sortedKeys = studentsMap.keys.toList()..sort();
+          studentsMap = Map.fromEntries(
+              sortedKeys.map((key) => MapEntry(key, studentsMap[key]!)));
 
-          return SliverGrid(
-            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                crossAxisCount: MediaQuery.of(context).size.width ~/ 160),
+          return SliverList(
             delegate: SliverChildBuilderDelegate(
               (BuildContext context, int index) {
-                return verticalStudentCard(students[index]);
+                return buildRoomCard(studentsMap.values.elementAt(index));
               },
-              childCount: students.length,
+              childCount: studentsMap.length,
             ),
           );
+
+          // return Placeholder();
         }
       },
     );
@@ -293,118 +295,135 @@ class _StudentsPageState extends State<StudentsPage> {
     final clr = Theme.of(context).colorScheme;
     return Padding(
       padding: const EdgeInsets.all(8.0),
-      child: InkWell(
-        onTap: () {
-          showDialog(
-            context: context,
-            builder: (BuildContext context) {
-              return ContactCardDialog(student: s);
-            },
-          );
-        },
-        child: LayoutBuilder(
-          builder: (BuildContext context, BoxConstraints constraints) {
-            final h = constraints.maxHeight;
-            final w = constraints.maxWidth;
-
-            return Card(
-              surfaceTintColor: s.toColor(),
-              shadowColor: s.toColor(),
-              elevation: 4,
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(24),
-              ),
-              child: Stack(children: [
-                RandomPersonIcon(size: h, color: clr.surface),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.only(top: 8, left: 8),
-                      child: Container(
-                        decoration: BoxDecoration(
-                            border: Border.all(color: clr.surface, width: 1),
-                            borderRadius: BorderRadius.circular(8),
-                            color: clr.onPrimary),
-                        child: Row(children: [
-                          Icon(Icons.bed_outlined, size: w * 0.11),
-                          Text(s.room.toString(),
-                              style: TextStyle(
-                                  fontWeight: FontWeight.bold,
-                                  fontSize: w * 0.09)),
-                        ]),
-                      ),
-                    ),
-                    IconButton(
-                      onPressed: () => showEditStudentDialog(context, s),
-                      icon: Icon(Icons.edit_outlined, size: h * 0.15),
-                      tooltip: 'Edit',
-                    ),
-                  ],
-                ),
-                Positioned(
-                  top: h * 0.45,
-                  child: Container(
-                    height: h * 0.5,
-                    width: w * 0.95,
-                    decoration: BoxDecoration(
-                        borderRadius: BorderRadius.only(
-                            bottomLeft: Radius.circular(24),
-                            bottomRight: Radius.circular(24)),
-                        gradient: LinearGradient(
-                          colors: [
-                            Colors.black.withAlpha(1),
-                            Colors.black.withAlpha(120)
-                          ],
-                          begin: Alignment.topCenter,
-                          end: Alignment.bottomCenter,
-                        )),
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.end,
-                      children: [
-                        Text(s.name,
-                            style: TextStyle(
-                                fontSize: w * 0.1, color: clr.primary)),
-                        Text(s.id,
-                            style: TextStyle(color: clr.primaryContainer)),
-                      ],
-                    ),
-                  ),
-                ),
-              ]),
+      child: AspectRatio(
+        aspectRatio: 1,
+        child: InkWell(
+          onTap: () {
+            showDialog(
+              context: context,
+              builder: (BuildContext context) {
+                return ContactCardDialog(student: s);
+              },
             );
           },
+          child: LayoutBuilder(
+            builder: (BuildContext context, BoxConstraints constraints) {
+              final h = constraints.maxHeight;
+              final w = constraints.maxWidth;
+
+              return Card(
+                surfaceTintColor: s.toColor(),
+                shadowColor: s.toColor(),
+                elevation: 4,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(24),
+                ),
+                child: Stack(children: [
+                  RandomPersonIcon(size: h, color: clr.surface),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      // Padding(
+                      //   padding: const EdgeInsets.only(top: 8, left: 8),
+                      //   child: Container(
+                      //     decoration: BoxDecoration(
+                      //         border: Border.all(color: clr.surface, width: 1),
+                      //         borderRadius: BorderRadius.circular(8),
+                      //         color: clr.onPrimary),
+                      //     child: Row(children: [
+                      //       Icon(Icons.bed_outlined, size: w * 0.11),
+                      //       Text(s.room.toString(),
+                      //           style: TextStyle(
+                      //               fontWeight: FontWeight.bold,
+                      //               fontSize: w * 0.09)),
+                      //     ]),
+                      //   ),
+                      // ),
+                      IconButton(
+                        onPressed: () => showEditStudentDialog(context, s),
+                        icon: Icon(Icons.edit_outlined, size: h * 0.13),
+                        tooltip: 'Edit',
+                      ),
+                      IconButton(
+                        onPressed: () => launchUrlString('tel:${s.contact}'),
+                        icon: Icon(Icons.phone_forwarded_outlined, size: h * 0.13),
+                        tooltip: 'call',
+                      ),
+                    ],
+                  ),
+                  Positioned(
+                    top: h * 0.45,
+                    child: Container(
+                      height: h * 0.5,
+                      width: w * 0.95,
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.only(
+                              bottomLeft: Radius.circular(24),
+                              bottomRight: Radius.circular(24)),
+                          gradient: LinearGradient(
+                            colors: [
+                              Colors.black.withAlpha(1),
+                              Colors.black.withAlpha(120)
+                            ],
+                            begin: Alignment.topCenter,
+                            end: Alignment.bottomCenter,
+                          )),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        children: [
+                          Text(s.name,
+                              style: TextStyle(
+                                  fontSize: w * 0.1, color: clr.primary)),
+                          Text(s.id,
+                              style: TextStyle(color: clr.primaryContainer)),
+                        ],
+                      ),
+                    ),
+                  ),
+                ]),
+              );
+            },
+          ),
         ),
       ),
     );
   }
 
   Widget buildRoomCard(List<Student> l) {
+    final clr = Theme.of(context).colorScheme;
+    final scr = MediaQuery.sizeOf(context);
     return Card(
       child: Container(
-        color: Colors.amber[200],
+        height: max(150, scr.width * 0.2),
+        // color: Colors.amber[200],
         child: Row(
           children: [
-            RotatedBox(
-              quarterTurns: -1,
-              child: Center(
-                child: Text(
-                  l[0].room.toString(),
-                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 30),
+            ClipRRect(
+              borderRadius: BorderRadius.only(
+                  bottomRight: Radius.circular(50),
+                  topRight: Radius.circular(50)),
+              child: Container(
+                color: l[0].toColor().withAlpha(30),
+                child: RotatedBox(
+                  quarterTurns: -1,
+                  child: Center(
+                    child: Text(
+                      l[0].room.toString(),
+                      style:
+                          TextStyle(fontWeight: FontWeight.bold, fontSize: 30),
+                    ),
+                  ),
                 ),
               ),
             ),
             Expanded(
-              child: SizedBox(
-                height: 150,
-                child: ListView.builder(
-                  scrollDirection: Axis.horizontal,
-                  itemCount: l.length,
-                  itemBuilder: (context, i) {
-                    return verticalStudentCard(l[i]);
-                  },
-                ),
+              child: ListView.builder(
+                scrollDirection: Axis.horizontal,
+                itemCount: l.length,
+                itemBuilder: (context, index) {
+                  return verticalStudentCard(l[index]);
+                },
               ),
             ),
           ],
